acd1756d2ef2d052b09f3b5ead8699042951208c
JocheitoBus <jocheitobus@gmail.com>
2026-05-15 05:14:00 -0400

    3

--- src/config/database.py
+++ src/config/database.py
@@ -4,8 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
+print(f"DEBUG: La URL cargada es: {os.getenv('DB_URL')}")
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://user:password@localhost:3306/db_name")
+DATABASE_URL = os.getenv("DB_URL", "mysql+pymysql://user:password@localhost:3306/db_name")
 
 engine = create_engine(
     DATABASE_URL, 
